966687588e29ced7e67ae315c7c9bdc8d7c21ae1
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 23:02:09 +0300

    create TelegramNotification

--- notifications/strategies.py
+++ notifications/strategies.py
@@ -1,6 +1,7 @@
 import logging
 from abc import ABC, abstractmethod
 
+import requests
 from django.core.mail import EmailMessage
 
 from config.settings import EMAIL_HOST_USER, TELEGRAM_URL, TELEGRAM_TOKEN
@@ -53,6 +54,63 @@ class EmailNotification(NotificationStrategy):
 
             except Exception as e:
                 logger.error(f"Error sending email {subject} to {recipient.pk}: {e}.")
+                results.append(recipient.pk)
+
+        return results
+
+
+class TelegramNotification(NotificationStrategy):
+    def send(
+        self, subject: str, message: str, recipient_list: list[Recipient]
+    ) -> list[int | None]:
+        results = []
+        for recipient in recipient_list:
+            tg_chat_id = recipient.tg_chat_id
+            if tg_chat_id:
+
+                payload = {
+                    "chat_id": tg_chat_id,
+                    "text": message,
+                }
+                try:
+                    response = requests.post(
+                        f"{TELEGRAM_URL}{TELEGRAM_TOKEN}/sendMessage", data=payload
+                    )
+                    response.raise_for_status()  # Вызовет исключение при ошибках HTTP (4xx или 5xx)
+                    data = response.json()
+
+                    if data.get("ok"):
+                        # Сообщение успешно принято сервером Telegram
+                        logger.info(
+                            f"Telegram message {subject} sent successfully to {recipient.pk}."
+                        )
+
+                    else:
+                        # Сервер Telegram вернул ошибку, но статус HTTP был 200 OK
+                        print(
+                            f"Telegram message {subject} delivery failed to {recipient.pk}: {data.get('description')}"
+                        )
+                        logger.error(
+                            f"Telegram message {subject} delivery failed to {recipient.pk}: {data.get('description')}"
+                        )
+                        results.append(recipient.pk)
+
+                except requests.exceptions.RequestException as e:
+                    # Ошибка сети, DNS, таймаут или HTTP-ошибка
+                    logger.error(
+                        f"Net/HTTP error sending telegram message {subject} to {recipient.pk}: {e}."
+                    )
+                    results.append(recipient.pk)
+                except Exception as e:
+                    # Другие непредвиденные ошибки
+                    logger.error(
+                        f"Unexpected error sending telegram message {subject} to {recipient.pk}: {e}."
+                    )
+                    results.append(recipient.pk)
+            else:
+                logger.error(
+                    f"Telegram message {subject} delivery failed to {recipient.pk}: there is no tg_chat_id."
+                )
                 results.append(recipient.pk)
 
         return results
