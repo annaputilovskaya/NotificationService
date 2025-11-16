49ee2d6b8302a3ab363dca97aabf11c0e94c62c4
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:51:08 +0300

    create model Notification

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from recipients.models import Recipient
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Notification(models.Model):
+    """Модель уведомления."""
+
+    STATUS_CHOICES = [
+        ("CREATED", "Создано"),
+        ("IN_PROGRESS", "В процессе"),
+        ("COMPLETED", "Завершено"),
+    ]
+
+    topic = models.CharField(max_length=250, verbose_name="Тема", **NULLABLE)
+    text = models.TextField(verbose_name="Текст", **NULLABLE)
+    status = models.CharField(
+        max_length=30, choices=STATUS_CHOICES, default="CREATED", verbose_name="Статус"
+    )
+    recipients = models.ManyToManyField(
+        Recipient, verbose_name="Получатели",  **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = "Уведомление"
+        verbose_name_plural = "Уведомления"
