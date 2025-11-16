25905538e8414fe8caf63bacd9183d873fa9229d
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:18:44 +0300

    create model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    """
+    Модель пользователя.
+    """
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
