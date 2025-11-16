d60bac5588127a87102e4e6c61d4a2b791c47c6b
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:40:46 +0300

    create RecipientViewSet

--- recipients/views.py
+++ recipients/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from recipients.models import Recipient
+from recipients.serializers import RecipientSerializer
+
+
+class RecipientViewSet(ModelViewSet):
+    """
+    Контроллер получателя.
+    """
+
+    queryset = Recipient.objects.all()
+    serializer_class = RecipientSerializer
+    permission_classes = (IsAuthenticated,)
