d069f4252457c2927d1e3e5e46155b154a4e4d79
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:25:07 +0300

    create model UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
