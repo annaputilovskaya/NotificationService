e39cf6fdba9b07f0ecdb5e289eac42756332ea36
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:43:01 +0300

    create urls for recipients

--- recipients/apps.py
+++ recipients/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RecipientsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "recipients"
+    verbose_name = "Получатели"
