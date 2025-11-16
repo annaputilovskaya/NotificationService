90658f085482bd1e352c963ecb284e23acd49b6b
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-16 22:38:36 +0300

    create model Recipient

--- recipients/models.py
+++ recipients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Recipient(models.Model):
+    """
+    Модель получателя.
+    """
+
+    first_name = models.CharField(max_length=100, verbose_name="Имя", **NULLABLE)
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия", **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name="Электронная почта")
+    phone = PhoneNumberField(verbose_name="Телефон", **NULLABLE)
+    tg_chat_id = models.CharField(
+        max_length=100, verbose_name="ID чата в Telegram", **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = "Получатель"
+        verbose_name_plural = "Получатели"
+
+    def __str__(self):
+        return f"{self.email}"
