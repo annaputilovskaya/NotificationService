5400b246c926e2155c36584a1efc44ffd34c399f
Anna_Putilovskaya <putilovakayaa@mail.ru>
2025-11-17 12:23:05 +0300

    add urls for NotificationListAPIView and NotificationRetrieveAPIView

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from notifications.apps import NotificationsConfig
-from notifications.views import NotificationCreateAPIView
+from notifications.views import (
+    NotificationCreateAPIView, NotificationListAPIView, NotificationRetrieveAPIView,
+)
 
 app_name = NotificationsConfig.name
 
 urlpatterns = [
-    path("create/", NotificationCreateAPIView.as_view(), name="notification-create"),
+    path("create/", NotificationCreateAPIView.as_view(), name="create"),
+    path("list/", NotificationListAPIView.as_view(), name="list"),
+    path("read/", NotificationRetrieveAPIView.as_view(), name="read"),
 ]
